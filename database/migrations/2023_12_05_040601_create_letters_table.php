1285197bda3acdb3ce3023a85ad69ac5aa1a5d42
Fdvky1 <fdvkey@gmail.com>
2023-12-05 14:21:47 +0700

    feat: add letter migration

---
+++ database/migrations/2023_12_05_040601_create_letters_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('letters', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamp('letter_date');
+            $table->timestamp('received_date');
+            $table->string('summary')->default('');
+            $table->string('letter_number');
+            $table->string('information');
+            $table->string('from');
+            $table->string('to');
+            $table->enum('type', ['incoming', 'outgoing']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('letters');
+    }
+};
