43165c37c557558e9270b15b02ac64cb9e7ec400
Fuji Halim Rabbani <racun1601@gmail.com>
2023-12-08 05:58:29 +0700

    feat: add old input text

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -19,34 +19,31 @@
                     <div class="card-header">Create User</div>
 
                     <div class="card-body">
-                        <form method="POST" action="{{ route('users.store') }}">
-                            @csrf
+                    <form method="POST" action="{{ route('users.store') }}">
+                        @csrf
 
-                            <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">First Name</label>
-
-                                <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control" name="name" required autofocus>
-                                </div>
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">First Name</label>
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
                             </div>
+                        </div>
 
-                            <div class="form-group row">
-                                <label for="last_name" class="col-md-4 col-form-label text-md-right">Last Name</label>
-
-                                <div class="col-md-6">
-                                    <input id="last_name" type="text" class="form-control" name="last_name" required>
-                                </div>
+                        <div class="form-group row">
+                            <label for="last_name" class="col-md-4 col-form-label text-md-right">Last Name</label>
+                            <div class="col-md-6">
+                                <input id="last_name" type="text" class="form-control" name="last_name" value="{{ old('last_name') }}" required>
                             </div>
+                        </div>
 
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email" required>
-                                </div>
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
                             </div>
+                        </div>
 
-                            <div class="form-group row">
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -60,30 +57,27 @@
                             </div>
                         </div>
 
-
-
-
-                            <div class="form-group row">
-                                <label for="role" class="col-md-4 col-form-label text-md-right">Role</label>
-
-                                <div class="col-md-6">
-                                    <select id="role" class="form-control" name="role" required>
-                                        <option value="user">User</option>
-                                        <option value="admin">Admin</option>
-                                        <option value="superadmin">Super Admin</option>
-                                    </select>
-                                </div>
+                        <div class="form-group row">
+                            <label for="role" class="col-md-4 col-form-label text-md-right">Role</label>
+                            <div class="col-md-6">
+                                <select id="role" class="form-control" name="role" required>
+                                    <option value="user">User</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="superadmin">Super Admin</option>
+                                </select>
                             </div>
+                        </div>
 
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Create User
-                                    </button>
-                                </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Create User
+                                </button>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
+                </div>
+
                 </div>
             </div>
         </div>
