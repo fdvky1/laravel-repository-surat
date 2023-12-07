3825e5be275143a0d0b2afd2bb4286efcd2af9e5
Fuji Halim Rabbani <racun1601@gmail.com>
2023-12-07 17:22:39 +0700

    fix: image not responsive

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -263,8 +263,12 @@
                     <!-- Nav Item - User Information -->
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
-                            <figure class="img-profile rounded-circle avatar font-weight-bold" data-initial="{{ Auth::user()->name[0] }}"></figure>
+                            <span class="mr-2 d-none d-lg-inline text-gray-600 ">{{ Auth::user()->name }}</span>
+                            @if (Auth::user()->profile_photo)
+                                <img src="{{ asset('storage/profiles/' . Auth::user()->profile_photo) }}" class="img-fluid rounded-circle avatar font-weight-bold" alt="">
+                            @else
+                                <figure class="img-profile rounded-circle avatar font-weight-bold" data-initial="{{ Auth::user()->name[0] }}"></figure>
+                            @endif
                         </a>
                         <!-- Dropdown - User Information -->
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
